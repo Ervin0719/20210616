e30b12dd508e89b4fcecf0958163efccbcc8ce55
Ervin <o1083011@gm.pu.edu.tw>
2021-06-16 22:35:38 +0800

    第一次稿圖 20210616

---
+++ app/src/main/java/tw/edu/pu/o1083011/a20210616/PlayerActivity.kt
@@ -0,0 +1,11 @@
+package tw.edu.pu.o1083011.a20210616
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PlayerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_player)
+    }
+}
